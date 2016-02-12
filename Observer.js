d388cc7e14a5946078c2c71008d6022ef833aef6
Gennadii Tarasenko <gennadii.tarasenko@gmail.com>
2016-02-13 05:46:06 +0800

    few classes implemented

--- Observer.js
+++ Observer.js
@@ -11,11 +11,13 @@ var Weather = function(){
             if(data.view) this.view = data.view;
             this.notifyObservers();
     }
+    //broadcasting "ONE TO MANY"
     this.notifyObservers = function(){
         for(var i = 0; i < this.subscribers.length; i++){
             this.subscribers[i].update(this.temp, this.humidity, this.view);
         }
     }
+    
     this.subscribe = function(subscriber){
         if(subscriber.update){
             this.subscribers.push(subscriber);
@@ -24,6 +26,7 @@ var Weather = function(){
             console.log("Can't subscribe! Method 'update(t,h,v) must be implemented!'");
         }
     }
+    
     this.unsubscribe = function(subscriber){
         var i = this.subscribers.indexOf(subscriber);
         if(i!==-1){
@@ -32,6 +35,7 @@ var Weather = function(){
         }
     }
 }
+
 //observer
 var DisplayService = function(name){
     this.name = name;
@@ -39,14 +43,28 @@ var DisplayService = function(name){
         console.log(name + " says: temp = " + t + ", humidity = " + h + " and it is " + v + "! Have a nice day and watch "+ this.name +" :)");
     }
 }
+//bad observer
+var DisplayServiceWrong = function(name){
+    this.name = name;
+    this.up = function(t,h,v){
+        console.log("temp = " + t + ", hum = " + h + " and it is " + v + "!");
+    }
+}
+//another observer
+var DisplayServiceUA = function(name){
+    this.name = name;
+    this.update = function(t,h,v){
+        console.log(name + " говорить: температура = " + t + ", вологiсть = " + h + " i зараз " + v + "! Доброго дня i дивiться "+ this.name +" :)");
+    }
+}
 
-
-
+//run
 var weather = new Weather();
 var zdf = new DisplayService("ZDF");
-var opo = new DisplayService("1+1");
+var opo = new DisplayServiceUA("1+1");
 var bbc = new DisplayService("BBC");
 var cnn = new DisplayService("CNN");
+var bad = new DisplayServiceWrong("bad");
 
 weather.subscribe(zdf);
 weather.subscribe(opo);
@@ -55,4 +73,5 @@ weather.update({temp:23});
 weather.unsubscribe(zdf);
 weather.update({humidity:100});
 weather.subscribe(bbc);
+weather.subscribe(bad);
 weather.update({view:"raining"});
