3b1c82b7e59ee00b18521fb74febe30885c0a091
Gennadii Tarasenko <gennadii.tarasenko@gmail.com>
2016-02-15 14:32:14 +0800

    getter-setter ready

--- get_set.js
+++ get_set.js
@@ -1,4 +1,5 @@
 //http://habrahabr.ru/company/enterra/blog/153365/
+var mikhail = {};
 Object.defineProperty(mikhail, 'first_name', { value:    'Mikhail'
                                              , writable: true })
 
@@ -13,10 +14,10 @@ function get_full_name() {
 
 // (new_name:String) → undefined
 // Sets the name components of the object, from a full name.
-function set_full_name(new_name) { var names
-    names = new_name.trim().split(/\s+/)
-    this.first_name = names[⁣'0'] || ''
-    this.last_name  = names['1'] || ''
+function set_full_name(new_name) { 
+    var names = new_name.trim().split(/\s+/);
+    this.first_name = names['0'] || '';
+    this.last_name  = names['1'] || '';
 }
 
 Object.defineProperty(mikhail, 'name', { get: get_full_name
@@ -36,3 +37,5 @@ console.log(mikhail.last_name);
 console.log(mikhail.last_name = 'White');
 console.log(mikhail.name);
 // => 'Mikhail White'
+mikhail.name = 'Peter Schwarz';
+console.log(mikhail.name);
