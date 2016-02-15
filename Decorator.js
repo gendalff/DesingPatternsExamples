2970a392faf526ef70b84f20548ff612b436f5b9
Gennadii Tarasenko <gennadii.tarasenko@gmail.com>
2016-02-15 15:25:08 +0800

    Broken Decorator

--- Decorator.js
+++ Decorator.js
@@ -4,25 +4,42 @@ The Decorator pattern attaches additional resposibilities to an object dynamical
 Decorators provide a flexible alternative to subclassing for extending functionality.
 */
 
-var Beverage = function(description, price, condiment){
-    this.description = description;
-    this.price = price;
-    this.condiment = condiment;
+var Beverage = function(){
+    this.description = "description";
+    this.price = 0;
     this.getCost = function(){
-        var total = this.price;
-        if(this.condiment) total += this.condiment.getCost();
-        return  total;
+        return  this.price;
     } 
     this.getDescription = function(){
-        var total = this.description;
-        if(this.condiment) total = this.condiment.getDescription() + total;
-        return  total;
+        return  this.description;
     } 
 }
+var Espresso = function(){
+    Beverage.call(this);
+    this.description = "Espresso";
+    this.price = 15;
+}
+
+var Condiment = function(beverage){
+    this.beverage = beverage;
+    this.getCost = function(){
+        var total = 
+    }
+}
+
+var Soy = function(){
+    Condiment.call(this);
+    this.description = ", soy";
+    this.price = 2;
+}
+
 
 var espresso = new Beverage("espresso", 15);
-espresso = new Beverage(", soy", 2, espresso);
-espresso = new Beverage(", milk", 3, espresso);
+var soy = new Decorator(", soy", 3);
+// espresso = new Beverage(", soy", 2, espresso);
+// espresso = new Beverage(", milk", 3, espresso);
 
 console.log(espresso.getCost());
 console.log(espresso.getDescription() + "makes total price: " + espresso.getCost() + "UAH");
+
+console.log(soy.getDescription());
