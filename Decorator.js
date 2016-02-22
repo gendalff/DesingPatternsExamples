7d1ab0866408cb9a31d7354775bba49b8e3eff90
Gennadii Tarasenko <gennadii.tarasenko@gmail.com>
2016-02-22 08:17:00 +0800

    Decorator is up

--- Decorator.js
+++ Decorator.js
@@ -14,32 +14,76 @@ var Beverage = function(){
         return  this.description;
     } 
 }
+
 var Espresso = function(){
     Beverage.call(this);
     this.description = "Espresso";
     this.price = 15;
 }
 
+var Tea = function(){
+    Beverage.call(this);
+    this.description = "Tea";
+    this.price = 10;
+}
+
+var Americano = function(){
+    Beverage.call(this);
+    this.description = "Americano";
+    this.price = 17;
+}
+
+
+Espresso.prototype = Object.create(Beverage.prototype);
+Espresso.prototype.constructor = Espresso;
+
 var Condiment = function(beverage){
     this.beverage = beverage;
     this.getCost = function(){
-        var total = 
+        return this.beverage.getCost() + this.price;
+    }
+    this.getDescription = function(){
+        return this.beverage.getDescription() + this.description;
     }
 }
 
-var Soy = function(){
-    Condiment.call(this);
+var Soy = function(beverage){
+    Condiment.call(this, beverage);
     this.description = ", soy";
     this.price = 2;
 }
 
+var Mocha = function(beverage){
+    Condiment.call(this, beverage);
+    this.description = ", mocha";
+    this.price = 5;
+}
 
-var espresso = new Beverage("espresso", 15);
-var soy = new Decorator(", soy", 3);
-// espresso = new Beverage(", soy", 2, espresso);
-// espresso = new Beverage(", milk", 3, espresso);
+var Milk = function(beverage){
+    Condiment.call(this, beverage);
+    this.description = ", milk";
+    this.price = 3;
+}
 
-console.log(espresso.getCost());
-console.log(espresso.getDescription() + "makes total price: " + espresso.getCost() + "UAH");
+function display(description, cost){
+    console.log(description + " makes total price: " + cost + "UAH");
+}
 
-console.log(soy.getDescription());
+Soy.prototype = Object.create(Condiment.prototype);
+Soy.prototype.constructor = Soy;
+
+ var order1 = new Espresso();
+ order1 = new Soy(order1);
+ order1 = new Mocha(order1);
+ 
+ var order2 = new Tea();
+ 
+ var order3 = new Americano();
+ order3 = new Milk(order3);
+ order3 = new Soy(order3);
+ order3 = new Mocha(order3);
+ order3 = new Mocha(order3);
+
+display(order1.getDescription(), order1.getCost());
+display(order2.getDescription(), order2.getCost());
+display(order3.getDescription(), order3.getCost());
---
+++ legacy/inheritance.js
@@ -0,0 +1,54 @@
+//implementing a ZOO
+
+var Mammal = function(name){
+    if(name) this.name = name;
+}
+    Mammal.prototype.bornChild = function(){
+        console.log(this.name + " borns a new Child");
+    }
+    Mammal.prototype.breastfeeding = function(){
+        console.log(this.name + " feeding with brest");
+    }
+
+var Herbivorous = function(name){
+    Mammal.call(this, name);
+    this.eat = this.chew;
+}
+    Herbivorous.prototype = Object.create(Mammal.prototype);
+    Herbivorous.prototype.chew = function(){
+        console.log(this. name + " eating some plant");
+    }
+    Herbivorous.prototype.constructor = Herbivorous;
+
+var Predator = function(name){
+    Mammal.call(this, name);
+    this.eat = this.bite;
+}
+
+    Predator.prototype = Object.create(Mammal.prototype);
+    Predator.prototype.constructor = Predator;
+    Predator.prototype.bite = function(){
+            console.log(this. name + " bites some animal");
+        };
+    Predator.prototype.kill = function(){
+        console.log("Kill when I'm Hungry");
+    };
+
+var Mixed = function(name){
+    Herbivorous.call(this, name);
+    Predator.call(this);
+}
+Mixed.prototype = Object.create(Herbivorous.prototype);
+for(var i in Predator.prototype) Mixed.prototype[i] = Predator.prototype[i];
+Mixed.prototype.constructor = Mixed;
+// Mixed.prototype = Predator.prototype;
+
+
+var ship = new Herbivorous("Ship");
+var wolf = new Predator("Wolf");
+var human = new Mixed("Eva");
+
+
+ship.chew(); ship.bornChild(); ship.eat(); ship.breastfeeding();
+wolf.bite(); wolf.bornChild(); wolf.eat(); wolf.kill();
+human.chew(); human.bite(); human.bornChild(); human.eat(); human.breastfeeding(); human.kill();
